ec80a92fb7afb265848b1d3d28fc15b4033b2b8d
Jose Gamboa <joalgamro2010@gmail.com>
2021-06-23 15:01:03 -0600

    C: Main components

---
+++ IC_WebApp/src/app/components/form-exercise/form-exercise.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-exercise',
+  templateUrl: './form-exercise.component.html',
+  styleUrls: ['./form-exercise.component.css']
+})
+export class FormExerciseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ IC_WebApp/src/app/components/user-selector/user-selector.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-user-selector',
+  templateUrl: './user-selector.component.html',
+  styleUrls: ['./user-selector.component.css']
+})
+export class UserSelectorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
