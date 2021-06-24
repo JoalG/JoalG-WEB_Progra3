82000cf39e4182ec51450b8c702d13064ebeb733
Jose Gamboa <joalgamro2010@gmail.com>
2021-06-23 19:11:42 -0600

    A: Exercise-card component

--- IC_WebApp/src/app/components/exercise-card/exercise-card.component.ts
+++ IC_WebApp/src/app/components/exercise-card/exercise-card.component.ts
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExerciseCardComponent implements OnInit {
 
+  name: string = "Arbol binario";
+  category: string = "Árboles"
+  author: string = "Diego Mora";
+  date: Date = new Date();
+  description: string = "Realice una función que retorne una lista que simboliza un árbol, por lo que se conforma por hijo derecha e izquierdo y el valor que simboliza la raíz. ";
+  difficulty: number = 3;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  counter(i: number) {
+    return new Array(i);
   }
 
 }
