e5e98d541db4181f8ed009aaf7d137a87c2ba07f
Joaquín Jiménez <joaquinenrique2010@gmail.com>
2021-06-25 10:29:12 -0600

    A: get exercises service

--- IC_WebApp/src/app/services/exercise.service.ts
+++ IC_WebApp/src/app/services/exercise.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { AngularFireDatabase } from '@angular/fire/database';
+import { AngularFireDatabase, AngularFireList } from '@angular/fire/database';
 import { Exercise } from '../models/exercise.model';
 
 @Injectable({
@@ -7,15 +7,55 @@ import { Exercise } from '../models/exercise.model';
 })
 export class ExerciseService {
 
+  exercises!: AngularFireList<any>;
   constructor(private database: AngularFireDatabase,) { }
 
   currentExercise!: Exercise;
 
   getExerciseByKey(key: string){
-    return this.database.database.ref('/exercises').child(key)
+    return this.database.database.ref('/exercises').child(key);
   }
 
   saveNewExercise(exercise: Exercise){
     this.database.database.ref('/exercises').push(exercise).then(response => {console.log(response)}, error => console.log(error));
   }
+
+  getExercises(){
+    return this.exercises = this.database.list('exercises');
+  }
+  /*
+  How to use getExercises in other components:
+
+    return this.exerciseService.getExercises()
+      .snapshotChanges().subscribe(item => {
+        this.exercises = [];
+        item.forEach(element => {
+          let x = element.payload.toJSON();
+          x["$key"] = element.key;
+          this.exercises.push(x as Exercise);
+        });
+        // Code that uses this.exercises
+      });
+
+
+  With promises:
+  
+    let exPromise = new Promise((resolve, reject)=>{
+      this.exerciseService.getExercises().snapshotChanges().subscribe(item => {
+        let exercises: Exercise[] = [];
+        item.forEach(element => {
+          let x = element.payload.toJSON();
+          exercises.push(x as Exercise);
+        });
+        resolve(exercises)
+      });
+    })
+
+    exPromise.then((data)=>{
+      this.exercises = <Exercise[]>data;
+      // Code that uses this.exercises
+      console.log(this.exercises);
+    })
+
+  */
 }
